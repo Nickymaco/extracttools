d4a205d05c8eacd4f44d73be2628d4a6c9b34f09
Nikymaco <john.ho.q@gmail.com>
2020-05-14 01:08:44 +0800

    get back unzip command

--- autoextract.sh
+++ autoextract.sh
@@ -182,20 +182,20 @@ extract_file() {
     local exec_cmd
 
     if [[ -f $exclude_file ]]; then
-        #if [[ "$1" == *'.zip' ]]; then
-        #    excludes=" -x "$(sed ':a ; N;s/\n/ / ; t a ; ' "$exclude_file")
-        if [[ "$1" == *'.rar' ]]; then
+        if [[ "$1" == *'.zip' ]]; then
+           excludes=" -x "$(sed ':a ; N;s/\n/ / ; t a ; ' "$exclude_file")
+        elif [[ "$1" == *'.rar' ]]; then
             excludes=" -x@\"$exclude_file\""
-        elif [[ "$1" == *'.7z' || "$1" == *'.zip' ]]; then
+        elif [[ "$1" == *'.7z' ]]; then
             excludes=" -x@\"$exclude_file\""
         fi
     fi 
 
     if [[ "$1" == *'.rar' ]]; then
         exec_cmd="unrar -or -p\"$2\"$excludes e \"$1\" ${exts_parrtern[*]} \"$4\""
-    #elif [[ "$1" == *'.zip' ]]; then
-    #    exec_cmd="unzip -P$2 -Ocp936 -j \"$1\" ${exts_parrtern[*]} $excludes  -d \"$4\""
-    elif [[ "$1" == *'.7z' || "$1" == *'.zip' ]]; then
+    elif [[ "$1" == *'.zip' ]]; then
+       exec_cmd="unzip -P$2 -Ocp936 -j \"$1\" ${exts_parrtern[*]} $excludes  -d \"$4\""
+    elif [[ "$1" == *'.7z' ]]; then
         exec_cmd="7za -p\"$2\" -o\"$4\"$excludes e \"$1\" ${exts_parrtern[*]} -sccUTF-8 -aot -r"
     else
         echo 'unkonw file'
